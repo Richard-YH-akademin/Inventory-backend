06d613ddd9e15696b42b2919b7867ebf4a6ad1fe
Susanna L <sanna.ljung@proton.me>
2026-03-05 15:00:27 +0100

    Added inventory endpoint, added gitignore

--- server.js
+++ server.js
@@ -5,9 +5,23 @@ const app = express();
  
 app.use(cors());
 app.use(express.json());
+
+let inventory = [
+
+  { id: 1, name: "Laptop", quantity: 5 },
+
+  { id: 2, name: "Keyboard", quantity: 10 }
+
+];
  
 app.get("/", (req, res) => {
   res.send("API fungerar");
+});
+
+app.get("/api/inventory", (req, res) => {
+  console.log("Inventory route called");
+  res.json(inventory);
+
 });
  
 app.listen(3000, () => {
